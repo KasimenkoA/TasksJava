df98a29ac4f1e8e2519bc2bb7d49d412e8ff3fd1
KasimenkoA <kasimenko@rambler.ru>
2023-04-07 20:49:42 +1000

    Создать случайное число. Вывести случайное число на экран.
    Увеличить это число в 3 раза.
    Вывести результат на экран.

---
+++ src/Task1.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Task1
+    {
+        public static void main( String[] args )
+            {
+                Random random = new Random();
+                int nn = random.nextInt(100);
+                System.out.println(nn);
+
+                int nn_new = nn * 3;
+                System.out.println(nn_new);
+            }
+    }
