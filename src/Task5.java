d63c074de9cef2960e6fe0677a2f7fdc87ffd577
KasimenkoA <kasimenko@rambler.ru>
2023-04-13 20:54:48 +1000

    Task № 5 Вывести все числа от 1 до 100, с помощью оператора fori. Число 95 не выводить - пропустить с помощью команды: continue; //продолжить

---
+++ src/Task5.java
@@ -0,0 +1,11 @@
+public class Task5
+    {
+        public static void main( String[] args )
+            {
+                for (int i = 1; i < 101; i++)
+                    {
+                        if (i == 95) continue;
+                        System.out.println(i);
+                    }
+            }
+    }
