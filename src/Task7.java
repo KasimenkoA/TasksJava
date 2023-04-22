7177ed8ba2a14ae355615298b2fca404964db7c5
KasimenkoA <kasimenko@rambler.ru>
2023-04-22 19:49:30 +1000

    Task № 7 Объявить переменную nn_Average. Вывести 10 случайных чисел. Найти среднее этих чисел и поместить в переменную nn_Average. Вывести на экран.

---
+++ src/Task7.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Task7
+    {
+        public static void main( String[] args )
+            {
+                int nn_Sum = 0;
+                int nn_Cur;
+                int nn_Average;
+
+                Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                    {
+                        nn_Cur = random.nextInt(100);
+                        nn_Sum += nn_Cur;
+                        System.out.println(nn_Cur);
+                    }
+
+                nn_Average = nn_Sum / 10;
+                System.out.println("Average: " + nn_Average);
+            }
+    }
