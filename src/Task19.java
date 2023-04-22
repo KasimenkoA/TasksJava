b6abf802e58e2ea3162a4432234f1baac5f1a275
KasimenkoA <kasimenko@rambler.ru>
2023-04-22 20:02:01 +1000

    Task № 19 Объявить числовую переменную sum. Создать цикл fori на 30 итераций. i Внутри этого цикла создать цикл на 20 итераций. j Внутри этого цикла создать цикл на 10 итераций. k Посчитать общую сумму произведения индексов: i * j * k

---
+++ src/Task19.java
@@ -0,0 +1,20 @@
+public class Task19
+    {
+        public static void main( String[] args )
+            {
+                int sum = 0;
+
+                for (int i = 0; i < 30; i++)
+                    {
+                        for (int j = 0; j < 20; j++)
+                            {
+                                for (int k = 0; k < 10; k++)
+                                    {
+                                        sum += i*j*k;
+                                    }
+                            }
+                    }
+
+                System.out.println(sum);
+            }
+    }
