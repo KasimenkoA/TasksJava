667f5e24cb493adf2ac1e23951616a4928e4593e
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:59:06 +1000

    Task № 59 В класс Pupil добавить реквизит типа Bag. В конструкторе класса Pupil заполнить bag случайным объектом Bag из MyCollections. В функции getInfo добавить информацию по портфелю. В классе task создать ученика и вывести по нему информацию.

---
+++ src/Task59.java
@@ -0,0 +1,12 @@
+import School.Pupil;
+
+public class Task59
+    {
+        public static void main( String[] args )
+            {
+                Pupil pupil = new Pupil();
+                pupil.getInfo();
+            }
+    }
+
+
