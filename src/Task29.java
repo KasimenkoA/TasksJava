1a7e488c086efdb9e40104f529ca340a28c994ee
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:47:39 +1000

    Task № 29 Объявить переменную xx. Объявить переменную yy. Присвоить xx случайное число до 100. Вывести xx на экран. Создать функцию расчета yy от xx: yy = getX( xx ); по формуле 2*x + 37 Вывести результат yy на экран.

---
+++ src/Task29.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Task29
+    {
+        public static void main( String[] args )
+            {
+                Random random = new Random();
+
+                int xx = random.nextInt(100);
+                System.out.println(xx);
+
+                int yy = getX(xx);
+                System.out.println(yy);
+            }
+
+        private static int getX( int xx )
+            {
+                return (2*xx + 37);
+            }
+    }
