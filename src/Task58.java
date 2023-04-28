d3d2e67d89fae64f42a2722bb9409103f8044a45
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:58:46 +1000

    Task № 58 В классе MyCollections создать функцию getRandomBag. Которая возвращает объект Bag со случайным цветом и фирмой. (использовать Random). В классе task получить объект Bag из MyCollections. Вывести результат на экран.

---
+++ src/Task58.java
@@ -0,0 +1,13 @@
+import MyMethods.MyCollections;
+import School.Bag;
+
+public class Task58
+    {
+        public static void main( String[] args )
+            {
+                Bag bag = MyCollections.getRandomBag();
+                bag.getInfo();
+            }
+    }
+
+
