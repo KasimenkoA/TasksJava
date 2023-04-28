51a33d0bfe21eeda17da862881bcbf90c1f8e1b7
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:53:21 +1000

    Task № 44 В классе MyCollections создать функцию public static int getRandomFromTo(int i_from, int i_to). Использовать класс Random. В классе task через эту функцию получить случайное число от 60 до 100. Вывести на экран.

---
+++ src/Task44.java
@@ -0,0 +1,10 @@
+import MyMethods.MyCollections;
+
+public class Task44
+    {
+        public static void main( String[] args )
+            {
+                int rr = MyCollections.getRandomFromTo( 60,100 );
+                System.out.println(rr);
+            }
+    }
