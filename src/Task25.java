0ed20273aea94b578706a411bfb1b5745c852e90
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:44:57 +1000

    Task № 25 Создать сканер. Объявить число nn. Запустить цикл while (true). Внутри цикла запросить у пользователя число. Если число равно 9, то написать "Выход." и вызвать команду break (прервать). Иначе вывести текст: "Следующее число: " и вывести следующее число, введенное пользователем.

---
+++ src/Task25.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Task25
+    {
+        public static void main( String[] args )
+            {
+                Scanner scanner = new Scanner( System.in );
+                int nn;
+
+                while (true)
+                    {
+                        System.out.print("Введите число: ");
+                        nn = scanner.nextInt();
+
+                        if (nn == 9)
+                            {
+                                System.out.println("Выход.");
+                                break;
+                            }
+                        else {
+                            nn++;
+                            System.out.println("Следующее число: " + nn);
+                        }
+                    }
+            }
+    }
