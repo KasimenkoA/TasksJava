27c9accede6a23e183adcd60c89bb9ccd6009c0c
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:57:59 +1000

    Task № 56 В классе MyCollections создать фунцию getFirms (Фирмы), которая возвращает строковый массив из 10 элементов. Заполнить массив десятью основными фирмами: Berlingo Brauberg Centropen Hatber Erich Krause Herlitz Loop Ergoflex ZNATOK Tiger Family SkyName В классе Task получить эти фирмы в массив из класса MyCollections. Вывести на экран функцией foreach.

---
+++ src/Task56.java
@@ -0,0 +1,16 @@
+import MyMethods.MyCollections;
+
+public class Task56
+    {
+        public static void main( String[] args )
+            {
+                String[] mas = MyCollections.getFirms();
+
+                for (String s : mas)
+                    {
+                        System.out.println(s);
+                    }
+            }
+    }
+
+
