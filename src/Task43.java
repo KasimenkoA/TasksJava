2e6454b0a7ffb3f4b73a51a30057da52be1bc4ad
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:53:03 +1000

    Task № 43 Создать пакет (Package) School. В каталоге School создать класс Pupil с полями name и age. В программе task создать объект pupil1. Присвоить имя и возраст. Вывести на экран. Пример: Ученик Петя, возраст 10 лет.

---
+++ src/Task43.java
@@ -0,0 +1,13 @@
+import School.Pupil;
+
+public class Task43
+    {
+        public static void main( String[] args )
+            {
+                Pupil pupil = new Pupil();
+                pupil.name = "Степа";
+                pupil.age = 9;
+
+                System.out.println("Ученик " + pupil.name + ", возраст " + pupil.age + " лет.");
+            }
+    }
