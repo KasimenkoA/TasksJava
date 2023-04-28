844a3eb98cdae83eb689cf285c8c64225e5a67ff
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:58:21 +1000

    Task № 57 В каталоге School создать класс Bag (портфель) с полями color и firm. Создать функцию getInfo с выводом информации по портфелю: Пример: Портфель Erich Krause цвет зеленый. В программе task создать объект bag. Присвоить полям color и firm любые значения. Вывести на экран.

---
+++ src/Task57.java
@@ -0,0 +1,14 @@
+import School.Bag;
+
+public class Task57
+    {
+        public static void main( String[] args )
+            {
+                Bag bag = new Bag();
+                bag.color = "малиновый";
+                bag.firm = "Erich Krause";
+                bag.getInfo();
+            }
+    }
+
+
