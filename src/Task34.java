a5f0a76993d4e893ad83ab77929e8255208a96fc
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:49:43 +1000

    Task № 34 Создать класс Animal. Создать объекты animal1 и animal2 с именами "dog" и "cat". Задать возраст. Вывести animal1.name + " and " + animal2.name
    Пример:
    
    public class Task34
        {
               ...
        }
    
    class Animal
        {
            String name;
            int age;
        }

---
+++ src/Task34.java
@@ -0,0 +1,21 @@
+public class Task34
+    {
+        public static void main( String[] args )
+            {
+                Animal animal1 = new Animal();
+                animal1.name = "dog";
+                animal1.age = 3;
+
+                Animal animal2 = new Animal();
+                animal2.name = "cat";
+                animal2.age = 1;
+
+                System.out.println("" + animal1.name + " and " + animal2.name);
+            }
+    }
+
+class Animal
+    {
+        String name;
+        int age;
+    }
