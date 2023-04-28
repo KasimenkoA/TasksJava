884eba41e27b7b7cdb7bce42cee383cc5e549984
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:55:21 +1000

    Task № 49 В классе Pupil создать конструктор public Pupil() в котором задать случайное имя из MyCollections и случайный возраст. В классе Task создать ученика и вывести на экран информацию по нему.

---
+++ src/Task49.java
@@ -0,0 +1,10 @@
+import School.Pupil;
+
+public class Task49
+    {
+        public static void main( String[] args )
+            {
+                Pupil pupil = new Pupil();
+                pupil.getInfo();
+            }
+    }
