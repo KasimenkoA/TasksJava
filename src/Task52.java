62eb3c5d26b11e52d3d5fe6eb143a3ae5ba20793
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:56:29 +1000

    Task № 52 Создать массив ArrayList типа Pupil. Создать и добавить в массив 100 учеников. Создать массив ArrayList 2 типа Pupil. В цикле foreach отобрать в массив ArrayList 2 только учеников с именем Захар. Вывести учеников второго массива на экран.

---
+++ src/Task52.java
@@ -0,0 +1,28 @@
+import School.Pupil;
+
+import java.util.ArrayList;
+
+public class Task52
+    {
+        public static void main( String[] args )
+            {
+                ArrayList<Pupil> arrayList1 = new ArrayList<>();
+                for (int i = 0; i < 100; i++)
+                    {
+                        arrayList1.add( new Pupil() );
+                    }
+
+                ArrayList<Pupil> arrayList2 = new ArrayList<>();
+                for (Pupil pupil : arrayList1)
+                    {
+                        if (pupil.name.equals( "Захар" )) arrayList2.add( pupil );
+                    }
+
+                for (Pupil pupil : arrayList2)
+                    {
+                        pupil.getInfo();
+                    }
+            }
+    }
+
+
