04deb4a850de7d36877dcfb957404d85d208f271
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 21:51:41 +1000

    Task № 39 В классе MyCollections создать функцию getPupilsNames (получить учеников имена). В которой заполнить строковый массив из 20 элементов. Придумать 10 имен девочек и 10 имен мальчиков. Вывести из Task39 на экран.

---
+++ src/Task39.java
@@ -0,0 +1,14 @@
+import MyMethods.MyCollections;
+
+public class Task39
+    {
+        public static void main( String[] args )
+            {
+                String[] mas = MyCollections.getPupilsNames();
+
+                for (int i = 0; i < mas.length; i++)
+                    {
+                        System.out.println(mas[i]);
+                    }
+            }
+    }
