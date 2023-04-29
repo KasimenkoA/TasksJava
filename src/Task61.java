283d2456a05b951ea8b3b0ac64101de141023b3f
KasimenkoA <kasimenko@rambler.ru>
2023-04-29 10:13:59 +1000

    Task № 61 Создать объект класса File с путем "Test1.txt". Создать объект класса Scanner с параметром file. В цикле пока, если есть следующая строка в файле, прочитать следующую строку и вывести ее на экран.

---
+++ src/Task61.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Task61
+    {
+        public static void main( String[] args ) throws FileNotFoundException
+            {
+                File file = new File( "Test1.txt" );
+
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNext())
+                    {
+                        System.out.println(scanner.nextLine());
+                    }
+
+            }
+    }
