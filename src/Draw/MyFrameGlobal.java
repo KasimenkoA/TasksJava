5195ceefbfc432cfa1d9da1455b50af411d8960a
KasimenkoA <kasimenko@rambler.ru>
2023-05-10 21:37:02 +1000

    Task № 82 Цель создать в классе MyFrameGlobal класс MyActionListener, чтобы убрать инициализацию ActionListener в кнопке 1.
    Для этого:
    В класс MyFrameGlobal добавить в начало объявление переменных:
    panelDraw
    button1
    button2
    ...
    button8
    В конструкторе убрать задание типов этих переменных (PanelDraw и JButton).
    
    В классе MyFrameGlobal создать класс MyActionListener. И заимплементировать его от ActionListener.
    Переопределить метод actionPerformed:
    Если e. (Получить из события источник) равен кнопке button1
    тогда рисовать овал из panelDraw.
    В функции addActionListener на кнопке button1 заменить на нашего слушателя new MyActionListener().

--- src/Draw/MyFrameGlobal.java
+++ src/Draw/MyFrameGlobal.java
@@ -7,6 +7,16 @@ import java.awt.event.ActionListener;
 
 public class MyFrameGlobal extends JFrame
     {
+        PanelDraw panelDraw;
+        JButton button1;
+        JButton button2;
+        JButton button3;
+        JButton button4;
+        JButton button5;
+        JButton button6;
+        JButton button7;
+        JButton button8;
+
         public MyFrameGlobal()
             {
                 setTitle( "My global frame" );
@@ -17,47 +27,58 @@ public class MyFrameGlobal extends JFrame
                 JPanel panelButton = new JPanel();
                 panelButton.setLayout( new GridLayout(2,4) );
 
-                PanelDraw panelDraw = new PanelDraw();
+                panelDraw = new PanelDraw();
                 panelDraw.setPreferredSize(new Dimension(600,400));
 
-                JButton button1 = new JButton("Button 1");
-                button1.addActionListener( new ActionListener()
-                    {
-                        @Override
-                        public void actionPerformed( ActionEvent e )
-                            {
-                                panelDraw.drawMyOval();
-                            }
-                    } );
+                button1 = new JButton("Button 1");
+//                button1.addActionListener( new ActionListener()
+//                    {
+//                        @Override
+//                        public void actionPerformed( ActionEvent e )
+//                            {
+//                                panelDraw.drawMyOval();
+//                            }
+//                    } );
+                button1.addActionListener(new MyActionListener());
                 panelButton.add( button1 );
 
-                JButton button2 = new JButton(("Button 2"));
+                button2 = new JButton(("Button 2"));
                 panelButton.add( button2 );
 
-                JButton button3 = new JButton(("Button 3"));
+                button3 = new JButton(("Button 3"));
                 panelButton.add( button3 );
 
-                JButton button4 = new JButton(("Button 4"));
+                button4 = new JButton(("Button 4"));
                 panelButton.add( button4 );
 
-                JButton button5 = new JButton(("Button 5"));
+                button5 = new JButton(("Button 5"));
                 panelButton.add( button5 );
 
-                JButton button6 = new JButton(("Button 6"));
+                button6 = new JButton(("Button 6"));
                 panelButton.add( button6 );
 
-                JButton button7 = new JButton(("Button 7"));
+                button7 = new JButton(("Button 7"));
                 panelButton.add( button7 );
 
-                JButton button8 = new JButton(("Button 8"));
+                button8 = new JButton(("Button 8"));
                 panelButton.add( button8 );
 
+
                 add( panelButton , BorderLayout.NORTH);
-
-
                 add( panelDraw, BorderLayout.SOUTH );
             }
 
+        public class MyActionListener implements ActionListener
+            {
+                @Override
+                public void actionPerformed( ActionEvent e )
+                    {
+                        if (e.getSource() == button1)
+                            {
+                                panelDraw.drawMyOval();
+                            }
+                    }
+            }
 
 
         public static void main( String[] args )
@@ -65,6 +86,7 @@ public class MyFrameGlobal extends JFrame
                 MyFrameGlobal frame = new MyFrameGlobal();
                 frame.setVisible( true );
             }
+
 
         public static class PanelDraw extends JPanel
             {
