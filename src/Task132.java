431c9911422819851b6ae251a3b15f3c7cc0dc3c
KasimenkoA <kasimenko@rambler.ru>
2023-06-30 21:50:08 +1000

    Task № 132 Создать класс Astronaut (космонавт) с переменной "name". Создать класс Spaceship (космический корабль) с переменной "name", переменной astronaut1 и astronaut2. Создать метод showInfo. Пример: Spaceship: Apollo. Astonaut: Don. Astonaut: Boris. Создать два космических корабля с астронавтами. Вывести информацию по каждому кораблю.

---
+++ src/Task132.java
@@ -0,0 +1,45 @@
+public class Task132
+    {
+        public static void main( String[] args )
+            {
+                Spaceship spaceship1 = new Spaceship( "Apollo",new Astronaut( "Don" ),new Astronaut( "Boris" ) );
+                Spaceship spaceship2 = new Spaceship( "Jupiter",new Astronaut( "Tom" ),new Astronaut( "Henk" ) );
+
+                spaceship1.showInfo();
+                spaceship2.showInfo();
+            }
+    }
+
+class Astronaut
+    {
+        private String name;
+
+        public Astronaut( String name )
+            {
+                this.name = name;
+            }
+
+        public String getName()
+            {
+                return name;
+            }
+    }
+
+class Spaceship
+    {
+        private String name;
+        private Astronaut astronaut1;
+        private Astronaut astronaut2;
+
+        public Spaceship( String name, Astronaut astronaut1, Astronaut astronaut2 )
+            {
+                this.name = name;
+                this.astronaut1 = astronaut1;
+                this.astronaut2 = astronaut2;
+            }
+
+        public void showInfo()
+            {
+                System.out.println("Spaceship: " + name + ". Astonaut: " + astronaut1.getName() + ". Astonaut: " + astronaut2.getName() + ".");
+            }
+    }
