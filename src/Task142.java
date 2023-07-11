0b1dc869c343dc0bd0afd5f84d14bb8b2eb2ac13
KasimenkoA <kasimenko@rambler.ru>
2023-07-11 21:52:15 +1000

    Task № 142 Создайте программу, которая определяет, является ли год високосным. С помощью сканера запросить год. В високосном году количество дней равно 366, а не високосном - 365. Подсказка: год високосный, если он делится нацело на 4 и при этом не делится на 100. Если год делится нацело на 400, то год тоже является високосным. Программа должна вывести "Год [год] является високосным", иначе выведите "Год [год] не является високосным". (Например, если год равен 2020, программа должна вывести "Год 2020 является високосным").

---
+++ src/Task142.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task142
+    {
+        public static void main( String[] args )
+            {
+                Scanner scanner = new Scanner( System.in );
+                System.out.print("Введите год: ");
+                int nn = scanner.nextInt();
+
+                if ((nn % 4 == 0 && nn % 100 != 0) || (nn % 400 == 0))
+                    {
+                        System.out.printf("Год %d является високосным",nn);
+                    }
+                else
+                    {
+                        System.out.printf("Год %d не является високосным",nn);
+                    }
+            }
+    }
