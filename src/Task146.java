50ee54457b8e80edaddb46816baec4f71f1e838d
KasimenkoA <kasimenko@rambler.ru>
2023-07-16 21:46:50 +1000

    Task № 146 Синтаксис цикла while: while(Логическое выражение) {    //Операторы } Напиши программу, которая будет выводить на экран числа от 1 до 10, используя цикл while.

---
+++ src/Task146.java
@@ -0,0 +1,13 @@
+public class Task146
+    {
+        public static void main( String[] args )
+            {
+                int nn = 1;
+
+                while (nn <= 10)
+                    {
+                        System.out.println(nn);
+                        nn++;
+                    }
+            }
+    }
