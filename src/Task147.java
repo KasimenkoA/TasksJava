4d4825333ad4f0af7f16bf29208fc5a7d5b4ded2
KasimenkoA <kasimenko@rambler.ru>
2023-07-17 22:35:42 +1000

    Задача о таблице умножения: Создай программу, которая будет запрашивать у пользователя число, а затем выводить на экран таблицу умножения для этого числа от 1 до 10, используя цикл while. Например, если пользователь вводит 5, то программа должна напечатать:
    5 x 1 = 5
    5 x 2 = 10
    ...
    5 x 10 = 50

---
+++ src/Task147.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task147
+    {
+        public static void main( String[] args )
+            {
+                Scanner scanner = new Scanner( System.in );
+                System.out.print("¬ведите число: ");
+                int nn = scanner.nextInt();
+
+                int ii = 1;
+                int rr;
+                while (ii <= 10)
+                    {
+                        rr = nn * ii;
+                        System.out.printf("%5d\t * %5d\t = %5d\n", nn, ii, rr);
+                        ii++;
+                    }
+            }
+    }
