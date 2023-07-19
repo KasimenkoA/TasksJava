104a07fc9bf82907f38463dd5dbcec9bc5d81a07
KasimenkoA <kasimenko@rambler.ru>
2023-07-19 21:58:49 +1000

    Task № 149 Задача о пропусках: Создай программу, которая будет выводить на экран только четные числа от 1 до 20, используя цикл while. Используй оператор continue, чтобы пропустить вывод нечетных чисел.

--- src/Task149.java
+++ src/Task149.java
@@ -2,7 +2,8 @@ public class Task149
     {
         public static void main( String[] args )
             {
-                for (int i = 1; i < 21; i++)
+                int i = 1;
+                while (++i<=20)
                     {
                         if (i%2==1) continue;
 
