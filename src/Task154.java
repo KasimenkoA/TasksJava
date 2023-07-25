01569ea42b25960e33b946d9ad793876ef27b272
KasimenkoA <kasimenko@rambler.ru>
2023-07-25 21:56:00 +1000

    Task № 154 Используй цикл for. Подсчет факториала: Запроси у пользователя число. Напиши программу, которая вычисляет факториал этого числа с использованием цикла for.

---
+++ src/Task154.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task154
+    {
+        public static void main( String[] args )
+            {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Введите число: ");
+                int nn = scanner.nextInt();
+
+                int ff = 1;
+                for (int i = 1; i <= nn ; i++)
+                    {
+                        ff *= i;
+                    }
+
+                System.out.println("Факториал числа " + nn + " равен " + ff);
+            }
+    }
