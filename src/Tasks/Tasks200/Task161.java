3eca6d23e1dbf1c63cc2298b07ec5204b6e8adad
KasimenkoA <kasimenko@rambler.ru>
2023-08-02 21:24:47 +1000

    Task № 161 Создайте программу, которая будет выводить на экран все нечетные числа от 1 до 19. Для вывода использовать цикл do..while.
    Пример:
    int ii = 1;
    do {
        System.out.println( ii );
    } while (ii<=15);

---
+++ src/Tasks/Tasks200/Task161.java
@@ -0,0 +1,13 @@
+package Tasks.Tasks200;
+
+public class Task161
+    {
+        public static void main( String[] args )
+            {
+                int ii = 1;
+                do {
+                    if (ii % 2 == 1) System.out.println( ii );
+                    ii++;
+                } while (ii<=19);
+            }
+    }
