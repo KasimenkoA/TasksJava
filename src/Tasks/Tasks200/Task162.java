57273fd2ffb193c3efc65561791116c53640438e
KasimenkoA <kasimenko@rambler.ru>
2023-08-03 21:53:47 +1000

    Task № 162 Создайте программу, которая будет выводить на экран все числа от 10 до 1 с помощью цикла do..while.

---
+++ src/Tasks/Tasks200/Task162.java
@@ -0,0 +1,13 @@
+package Tasks.Tasks200;
+
+public class Task162
+    {
+        public static void main( String[] args )
+            {
+                int ii = 10;
+                do {
+                    System.out.println(ii);
+                    ii--;
+                } while (ii>0);
+            }
+    }
