0f7369481ffdc9425427d18719ebc16c9d533531
KasimenkoA <kasimenko@rambler.ru>
2023-08-05 22:01:55 +1000

    Task № 164 Создайте программу, которая будет выводить на экран все числа от 1 до 100, кратные 3, с помощью цикла do..while.

---
+++ src/Tasks/Tasks200/Task164.java
@@ -0,0 +1,18 @@
+package Tasks.Tasks200;
+
+public class Task164
+    {
+        public static void main( String[] args )
+            {
+                int ii = 1;
+
+                do
+                    {
+                        if (ii % 3 == 0)
+                            {
+                                System.out.println( ii );
+                            }
+                        ii++;
+                    } while (ii <= 100);
+            }
+    }
