6574e5ea0ae042ec9846f7898ab92f6f1413e7d2
KasimenkoA <kasimenko@rambler.ru>
2023-08-12 21:56:24 +1000

    Task № 171 Оператор continue (продолжить). Создайте программу для проверки чисел от 1 до 20. Выведите только четные числа и пропустите нечетные числа с помощью оператора continue.

---
+++ src/Tasks/Tasks200/Task171.java
@@ -0,0 +1,14 @@
+package Tasks.Tasks200;
+
+public class Task171
+    {
+        public static void main( String[] args )
+            {
+                for (int i = 1; i <= 20; i++)
+                    {
+                        if (i % 2 ==1) continue;
+
+                        System.out.println(i);
+                    }
+            }
+    }
