407c9d88a465571c9b173475aebf46455bcf6099
KasimenkoA <kasimenko@rambler.ru>
2023-08-17 22:02:56 +1000

    Task № 176 Оператор switch..case. Пример: int cc = 1;
    switch (cc)
        {
            case 1:
                System.out.println("Белый");
                break;
            case 2:
                System.out.println("Синий");
                break;
            case 3:
                System.out.println("Красный");
                break;
            default:
                System.out.println("Не определено");
        }
    
    Создайте программу, которая запрашивает у пользователя число от 1 до 7
    и выводит на экран день недели, соответствующий этому числу.

---
+++ src/Tasks/Tasks200/Task176.java
@@ -0,0 +1,40 @@
+package Tasks.Tasks200;
+
+import java.util.Scanner;
+
+public class Task176
+    {
+        public static void main( String[] args )
+            {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Введите число от 1 до 7: ");
+                int nn = scanner.nextInt();
+
+                switch (nn)
+                    {
+                        case 1:
+                            System.out.println("Понедельник");
+                            break;
+                        case 2:
+                            System.out.println("Вторник");
+                            break;
+                        case 3:
+                            System.out.println("Среда");
+                            break;
+                        case 4:
+                            System.out.println("Четверг");
+                            break;
+                        case 5:
+                            System.out.println("Пятница");
+                            break;
+                        case 6:
+                            System.out.println("Суббота");
+                            break;
+                        case 7:
+                            System.out.println("Воскресенье");
+                            break;
+                        default:
+                            System.out.println("Не определено");
+                    }
+            }
+    }
