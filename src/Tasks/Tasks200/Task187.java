7a6fb45f132cb23edc185034b8f163341c4eaa86
KasimenkoA <kasimenko@rambler.ru>
2023-08-28 21:46:25 +1000

    Task № 187 Метод Math.min() Возвращает минимальное значение из двух аргументов. Напр: int с = min(int a, int b);
    Получите в цикле 10 случайных чисел.
    И найдите с помощью Math.min() самое минимальное число.
    Выведите все случайные числа и результат.

--- src/Tasks/Tasks200/Task187.java
+++ src/Tasks/Tasks200/Task187.java
@@ -2,23 +2,23 @@ package Tasks.Tasks200;
 
 import java.util.Random;
 
-public class Task186
+public class Task187
     {
         public static void main( String[] args )
             {
-                double rr;
                 int nn;
                 Random random = new Random();
 
+                int mm = Integer.MAX_VALUE;
                 for (int i = 0; i < 10; i++)
                     {
-                        nn = random.nextInt(10_000);
+                        nn = random.nextInt(100);
                         System.out.println(nn);
-                        rr = nn / 1000.0;
-                        System.out.println(rr);
-                        rr = Math.round( rr * 100.0 ) / 100.0;
-                        System.out.println(rr);
-                        System.out.println();
+
+                        mm = Math.min( mm,nn );
                     }
+
+                System.out.println();
+                System.out.println(mm);
             }
     }
