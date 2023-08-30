09581913919a5cd11ac09ace509790f2bb93c202
KasimenkoA <kasimenko@rambler.ru>
2023-08-30 21:59:55 +1000

    Task № 189 Метод Math.pow() power - энергия, степень Напр. 2 в степени 3 = Math.pow(2,3) = 8
    Найти все степени числа 2
    от 1 до 10.

---
+++ src/Tasks/Tasks200/Task189.java
@@ -0,0 +1,15 @@
+package Tasks.Tasks200;
+
+public class Task189
+    {
+        public static void main( String[] args )
+            {
+                int nn = 2;
+                double rr;
+                for (int i = 1; i <= 10; i++)
+                    {
+                        rr = Math.pow( nn, i );
+                        System.out.println("" + nn + " pow " + i + " = " + rr);
+                    }
+            }
+    }
