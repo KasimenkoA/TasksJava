3460b9064c39daf28ce6e31e80e4385bf88f6324
KasimenkoA <kasimenko@rambler.ru>
2023-09-17 21:38:32 +1000

    Task № 207 Создайте переменные a и b типа int. С помощью собственной функции расчитайте сумму этих переменных.
    Пример функции:
    private static int ИМЯ_ФУНКЦИИ( int a, int b )
                {
                    return РЕЗУЛЬТА;
                }

---
+++ src/Tasks/Tasks250/Task207.java
@@ -0,0 +1,18 @@
+package Tasks.Tasks250;
+
+public class Task207
+    {
+        public static void main( String[] args )
+            {
+                int a = 5;
+                int b = 4;
+                int c = sum(a,b);
+
+                System.out.println(c);
+            }
+
+        private static int sum( int a, int b )
+            {
+                return a + b;
+            }
+    }
