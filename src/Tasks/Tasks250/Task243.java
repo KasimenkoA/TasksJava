acafdcb409fb1072a6e00fa7652eb06a69082624
KasimenkoA <kasimenko@rambler.ru>
2023-11-02 22:07:12 +1000

    Task № 243 Создайте окно JFrame с заголовком "Простое окно". Установите размер 400 х 300 Установите операцию закрытия по умолчанию JFrame.EXIT_ON_CLOSE. Установите видимость в true.

---
+++ src/Tasks/Tasks250/Task243.java
@@ -0,0 +1,14 @@
+package Tasks.Tasks250;
+
+import javax.swing.*;
+
+public class Task243
+    {
+        public static void main( String[] args )
+            {
+                JFrame jFrame = new JFrame("Simple frame");
+                jFrame.setSize( 400, 300 );
+                jFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+                jFrame.setVisible( true );
+            }
+    }
