4b166dadd9800d0d7f91ce5be342364619c5be4d
KasimenkoA <kasimenko@rambler.ru>
2023-12-19 22:47:38 +1000

    Создайте класс Plane264 с полями name и speed. Создайте конструктор, создайте геттеры. В функции main создайте коллекцию ArrayList из самолетов. В цикле добавьте в коллекцию 10 самолетов. Имя должно быть: Самолет1, Самолет2 и т.д. Скорость должна быть случайным числом до 8000.
    В цикле foreach выведите информацию о каждом самолете вида:
    Самолет1, скорость 2500

---
+++ src/Tasks/Tasks300/Task264.java
@@ -0,0 +1,47 @@
+package Tasks.Tasks300;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class Plane264
+    {
+        private String name;
+        private int speed;
+
+        public Plane264( int name, int speed )
+            {
+                this.name = "Plane" + name;
+                this.speed = speed;
+            }
+
+        public String getName()
+            {
+                return name;
+            }
+
+        public int getSpeed()
+            {
+                return speed;
+            }
+    }
+
+public class Task264
+    {
+        public static void main( String[] args )
+            {
+                ArrayList<Plane264> arrayList = new ArrayList<>();
+                int nn;
+                Random random = new Random();
+                nn = random.nextInt(1);
+
+                for (int i = 1; i < 11; i++)
+                    {
+                        arrayList.add( new Plane264( i, random.nextInt(8000) ) );
+                    }
+
+                for (Plane264 plane264 : arrayList)
+                    {
+                        System.out.println("" + plane264.getName() + ", speed: " + plane264.getSpeed());
+                    }
+            }
+    }
